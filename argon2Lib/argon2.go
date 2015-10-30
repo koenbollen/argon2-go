45b5bd1520555b6016d84f250ebb9ca79004aa83
Marc Grol <mgrol@xebia.com>
2015-10-30 14:14:32 +0100

    Copied Argon2 sources into local project and let cgo compile

--- argon2Lib/argon2.go
+++ argon2Lib/argon2.go
@@ -1,8 +1,6 @@
 package argon2Lib
 
-// #cgo CFLAGS: -I../../../xebia/Argon2/Source/C99/Argon2/ -I../../../xebia/Argon2/Source/C99/Blake2 -I../../../xebia/Argon2/Source/C99/Common
-// #cgo LDFLAGS: -L/Users/mgrol/src/go/src/github.com/xebia/Argon2/Build -largon2
-// #cgo amd64 386 CFLAGS: -DX86=1
+// #cgo CFLAGS: -I./Argon2/ -I./Blake2 -I./Common -std=c99 -pthread -O3 -Wall -msse4.1
 // #include <stdlib.h>
 // #include "hash.h"
 import "C"
@@ -16,4 +14,4 @@ func HashPassword(clearTextPassword string) string {
 	return C.GoString(chash)
 }
 
-// #cgo LDFLAGS: -L../../xebia/Argon2/Build -largon2
+//// #cgo amd64 386 CFLAGS: -DX86=1
